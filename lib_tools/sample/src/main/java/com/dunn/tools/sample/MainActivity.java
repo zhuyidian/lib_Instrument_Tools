d5073ce990f295ad9bdfe55c9d19dc5ff98da668
zhuyidian <zhuyidian@coocaa.com>
2021-11-23 00:26:31 +0800

    update

--- lib_tools/sample/src/main/java/com/dunn/tools/sample/MainActivity.java
+++ lib_tools/sample/src/main/java/com/dunn/tools/sample/MainActivity.java
@@ -3,9 +3,13 @@ package com.dunn.tools.sample;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +18,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
     }
+
 }
