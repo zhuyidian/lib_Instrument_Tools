82b511d84bea8bed034ddc3af2808d7c6d8decce
zhuyidian <zhuyidian@coocaa.com>
2021-11-26 14:10:18 +0800

    update

--- lib_tools/sample/src/main/java/com/dunn/tools/sample/MainActivity.java
+++ lib_tools/sample/src/main/java/com/dunn/tools/sample/MainActivity.java
@@ -2,6 +2,7 @@ package com.dunn.tools.sample;
 
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -20,6 +21,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                double count = 0;
+                while(true) {
+                    count++;
+                    Log.i(TAG, "send ---> thread=" + Thread.currentThread()+", count="+count);
+                    reply(Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(2000);  //1776
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                double count = 0;
+                while(true) {
+                    count++;
+                    Log.i(TAG, "send ---> thread=" + Thread.currentThread()+", count="+count);
+                    reply(Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(3000);  //1188
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
     @Override
@@ -27,4 +60,15 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+    public void reply(String threadName) {
+        Intent intent = new Intent();
+        intent.putExtra("params", threadName);
+        intent.setAction("coocaa.intent.action.remote.platform.reply");
+        intent.setPackage("com.dunn.tools.sample");
+        if (Build.VERSION.SDK_INT >= 26) {
+            startForegroundService(intent);
+        } else {
+            startService(intent);
+        }
+    }
 }
